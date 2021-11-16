8b0df46ecaa2870fe2fdd2f1f22f4d88a7a4ac3e
Yassine Fathi <hi@m4tt72.com>
2021-11-16 18:27:41 +0100

    chore: added more info to 'help' command

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -4,7 +4,7 @@ import packageJson from '../../../package.json';
 export const help = async (args: string[]): Promise<string> => {
   const commands = Object.keys(bin).sort().join('\n');
 
-  return `Available commands:\n\n${commands}`;
+  return `Available commands:\n${commands}\n\n[tab]\t command completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
 };
 
 export const echo = async (args: string[]): Promise<string> => {
