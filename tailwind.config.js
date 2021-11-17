88faf0890a8ddb13d1b070490bca579af46638ee
Yassine Fathi <hi@m4tt72.com>
2021-11-17 13:03:51 +0100

    feat: added custom colors from config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const { colors } = require('./config.json');
+
 module.exports = {
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -9,22 +10,7 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      light: {
-        background: '#FBF1C9',
-        foreground: '#3C3836',
-        yellow: '#D79921',
-        green: '#98971a',
-        gray: '#7C6F64',
-        blue: '#458588',
-      },
-      dark: {
-        background: '#262626',
-        foreground: '#ebdbb2',
-        yellow: '#D79921',
-        green: '#98971a',
-        gray: '#a89984',
-        blue: '#458588',
-      },
+      ...colors,
     },
     extend: {},
   },
