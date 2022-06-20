4db0786ff457dc35ebd0a0a96e1759d7f9fa6889
Bram Boeckx <bram.boeckx@student.uhasselt.be>
2022-06-20 21:00:40 +0200

    fixed stupid bugs

--- src/utils/bin/cowsay.ts
+++ src/utils/bin/cowsay.ts
@@ -3,12 +3,12 @@ import { getQuote } from '../../api';
 
 export const cowsay = async (args?: string[]): Promise<string> => {
   let output = '';
-  if (args && args.length > 1) {
-    output = args.join(' ');
-    return cow.say({ text: output });
-  } else {
+
+  if (args.length < 1 || args[0] === '') {
     const quote = (await getQuote()).quote;
     return cow.say({ text: quote });
+  } else {
+    output = args.join(' ');
+    return cow.say({ text: output });
   }
-
 };
