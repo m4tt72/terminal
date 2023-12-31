64add1c0b47704b5ee46e7d9c2e4b2e80d7b22eb
Yassine Fathi <yassine.fathi@trustedfamily.net>
2023-12-31 15:29:14 +0100

    Add localStorage support for history

--- src/stores/history.ts
+++ src/stores/history.ts
@@ -1,4 +1,10 @@
 import { writable } from 'svelte/store';
 import type { Command } from '../interfaces/command';
 
-export const history = writable<Array<Command>>([]);
+export const history = writable<Array<Command>>(
+  JSON.parse(localStorage.getItem('history') || '[]'),
+);
+
+history.subscribe((value) => {
+  localStorage.setItem('history', JSON.stringify(value));
+});
