7dfcc2ecb299c498e56d0f132c6ce7bbd748c909
Yassine Fathi <yassine.fathi@trustedfamily.net>
2023-12-31 15:56:13 +0100

    Update favicon and title in index.html, add Cascadia Code font, and remove unused SVG files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{svelte,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      caretColor: ['focus-visible'],
-    },
-  },
+  theme: {},
   plugins: [],
 };
