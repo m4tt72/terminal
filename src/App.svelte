7972dae47c474bec7e2df531a7d7815084fc0459
Yassine Fathi <yassine.fathi@trustedfamily.net>
2023-12-31 15:29:27 +0100

    Add History component to display command history and outputs

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
   import Ps1 from './lib/Ps1.svelte';
-  import { history } from './stores/history';
   import Input from './lib/Input.svelte';
+  import History from './lib/History.svelte';
 </script>
 
 <main class="h-full border-2 rounded-md border-green-600 p-4 overflow-auto">
-  {#each $history as { command, outputs }}
-    <div>
-      <div class="flex flex-row">
-        <Ps1 />
-
-        <p class="text-white px-3">{command}</p>
-      </div>
-
-      {#each outputs as output}
-        <p class="text-white">{output}</p>
-      {/each}
-    </div>
-  {/each}
+  <History />
 
   <div class="flex flex-row">
     <Ps1 />
---
+++ src/lib/History.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { history } from '../stores/history';
+  import Ps1 from './Ps1.svelte';
+</script>
+
+{#each $history as { command, outputs }}
+  <div>
+    <div class="flex flex-row">
+      <Ps1 />
+
+      <p class="text-white px-3">{command}</p>
+    </div>
+
+    {#each outputs as output}
+      <p class="text-white whitespace-pre-wrap">{output}</p>
+    {/each}
+  </div>
+{/each}
