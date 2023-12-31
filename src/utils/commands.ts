80e6c2520616ee6715291ee0197cde69e14be448
Yassine Fathi <yassine.fathi@trustedfamily.net>
2023-12-31 15:37:53 +0100

    Add new commands and update existing commands

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -7,12 +7,48 @@ export const commands: Record<string, (args: string[]) => Promise<string> | stri
   help: () => 'Available commands: ' + Object.keys(commands).join(', '),
   hostname: () => hostname,
   whoami: () => 'guest',
+  date: () => new Date().toLocaleString(),
+  vi: () => `why use vi? try 'emacs'`,
+  vim: () => `why use vim? try 'emacs'`,
+  emacs: () => `why use emacs? try 'vim'`,
+  echo: (args: string[]) => args.join(' '),
+  sudo: (args: string[]) => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+
+    return `Permission denied: unable to run the command '${args[0]}' as root.`;
+  },
+  repo: () => {
+    window.open(packageJson.repository.url, '_blank');
+
+    return 'Opening repository...';
+  },
   clear: () => {
     history.set([]);
 
     return '';
   },
-  echo: (args: string[]) => args.join(' '),
+
+  email: () => {
+    window.open(`mailto:${packageJson.author.email}`);
+
+    return `Opening mailto:${packageJson.author.email}...`;
+  },
+  donate: () => {
+    window.open(packageJson.funding.url, '_blank');
+
+    return 'Opening donation url...';
+  },
+  weather: async (args: string[]) => {
+    const city = args.join('+');
+
+    if (!city) {
+      return 'Usage: weather [city]. Example: weather casablanca';
+    }
+
+    const weather = await fetch(`https://wttr.in/${city}?ATm`);
+
+    return weather.text();
+  },
   curl: async (args: string[]) => {
     if (args.length === 0) {
       return 'curl: no URL provided';
@@ -38,9 +74,5 @@ export const commands: Record<string, (args: string[]) => Promise<string> | stri
 ╚═╝     ╚═╝     ╚═╝   ╚═╝      ╚═╝      ╚═╝  ╚══════╝ v${packageJson.version}
 
 Type 'help' to see list of available commands.
-
---
-The project is open-source 🎉 type 'repo' to check out the repository.
---
 `,
 };
